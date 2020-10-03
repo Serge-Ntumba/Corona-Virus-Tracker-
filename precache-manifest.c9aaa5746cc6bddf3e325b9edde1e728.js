3d1a1e7af0eb6e08fa5588a3176d3ec584a182c6
Serge Ntumba <s7ntumba@gmail.com>
2020-10-03 14:15:51 +0400

    Updates

---
+++ precache-manifest.c9aaa5746cc6bddf3e325b9edde1e728.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997a5df8599a587606db0b5837e662cf",
+    "url": "/Corona-Visus-Tracker-/index.html"
+  },
+  {
+    "revision": "b3a2382b1f813356913e",
+    "url": "/Corona-Visus-Tracker-/static/css/main.e8ff66cd.chunk.css"
+  },
+  {
+    "revision": "876a7f52c4da817abe86",
+    "url": "/Corona-Visus-Tracker-/static/js/2.8656a684.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Visus-Tracker-/static/js/2.8656a684.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a2382b1f813356913e",
+    "url": "/Corona-Visus-Tracker-/static/js/main.19ed8445.chunk.js"
+  },
+  {
+    "revision": "f21c998c52f60c5f1e66",
+    "url": "/Corona-Visus-Tracker-/static/js/runtime-main.5fdbaa7f.js"
+  },
+  {
+    "revision": "2ef5399cab9f428d8310faa8423b9244",
+    "url": "/Corona-Visus-Tracker-/static/media/image.2ef5399c.png"
+  }
+]);
